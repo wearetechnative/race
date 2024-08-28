08af2dd53bfeec8d81a2ac42cff45fad0a7656fd
Pim Snel <post@pimsnel.com>
2024-08-28 12:51:26 +0200

    work with ctrl-c

--- profsel.sh
+++ profsel.sh
@@ -23,12 +23,25 @@ console(){
   checkdeps "assumego"
   checkdeps "gum"
 
-  GROUP=`jsonify-aws-dotfiles | jq -r '[.config[].group]| unique| sort| .[]' | grep -v null | gum choose`
+  GROUP=`jsonify-aws-dotfiles | jq -r '[.config[].group]| unique| sort| .[]' | grep -v null | gum choose --height 25`
 
-  echo $GROUP
+  if [ -z "${GROUP}" ] || [ "${GROUP}" = "user aborted" ]; then
+    exit 1
+  fi
 
-  ACCOUNTTMP=`jsonify-aws-dotfiles | jq "[.config | with_entries(select(.value.group == \"$GROUP\")) | to_entries[] | {\"AWS Account\": .key, \"Role\": .value.role_arn}]" | dasel -r json -w csv | gum table -w 30,70`
+  #echo $GROUP
+
+  ACCOUNTTMP=`jsonify-aws-dotfiles | jq "[.config | with_entries(select(.value.group == \"$GROUP\")) | to_entries[] | {\"AWS Account\": .key, \"Role\": .value.role_arn}]" | dasel -r json -w csv | gum table -w 30,70 --height 25`
+
+  if [ -z "${ACCOUNTTMP}" ]; then
+    exit 1
+  fi
+
   ACCOUNT=`echo $ACCOUNTTMP | cut -d ',' -f 1`
+
+  if [ -z "${ACCOUNT}" ]; then
+    exit 1
+  fi
 
   GRANTED_ALIAS_CONFIGURED="true" assumego -c $ACCOUNT
 }
